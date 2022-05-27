08d6efccca34a72021817f5b2298cdf5cbeb78f4
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-05-27 15:16:10 +0700

    Menampilkan No. Registrasi pada document.writeln()

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const myName = "Acep Awaludin";
+const myId = "1522 3586 5100-97"
 console.log("Halo, namaku adalah " + myName);
 document.writeln(
-    `<h1 style="text-align: center; margin-top: 50vh;">Halo, namaku adalah ${myName}</h1>`
-    )
+    `<h1 style="text-align: center; margin-top: 50vh;">Halo, <span style="color: red">Namaku</span> adalah <span style="color: red">${myName}</span></h1>
+    <h1 style="text-align: center;">Adapun <span style="color: red">No. Registrasiku</span> adalah <span style="color: red">${myId}</span></h1>`
+    );
 alert("Halo, namaku adalah " + myName);

e5c586f1837643276edc8cf5a600bb59888e4c9e
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-05-27 15:14:53 +0700

    Menambahkan file index.js dengan fungsi untuk menampilkan: Halo, Namaku adalah Acep Awaludin dengan menggunakan tiga cara yaitu console.log, document.writeln, dan alert di mana ketiganya sama-sama menggunakan variabel myName untuk menampilkan nama Acep Awaludin

---
+++ index.js
@@ -0,0 +1,6 @@
+const myName = "Acep Awaludin";
+console.log("Halo, namaku adalah " + myName);
+document.writeln(
+    `<h1 style="text-align: center; margin-top: 50vh;">Halo, namaku adalah ${myName}</h1>`
+    )
+alert("Halo, namaku adalah " + myName);

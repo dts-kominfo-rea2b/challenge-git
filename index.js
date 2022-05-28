fb3f397238e77807bfb168afe76f13ff7421458b
hendrymei25 <70930111+hendrymei25@users.noreply.github.com>
2022-05-29 00:18:19 +0700

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title> Challenge-git </title>
-    <script>
-        document.write("<h1>Halo,!</h1>");
-        document.write("<hr>");
-        document.write("<p>Perkenalkan Nama Saya</p>");
-        document.write("adalah <b>Hendri Mei Singgih</b>")
-    </script>
-</head>
-<body>
-            
-
-</body>
-</html>
+document.write("Halo, Nama Saya adalah Hendri Mei Singgih");

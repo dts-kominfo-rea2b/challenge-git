7e799815b5eb36f82445c69177b7ba7a31b910ba
hendrymei25 <70930111+hendrymei25@users.noreply.github.com>
2022-05-28 23:50:11 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Challenge-git </title>
+    <script>
+        document.write("<h1>Halo,!</h1>");
+        document.write("<hr>");
+        document.write("<p>Perkenalkan Nama Saya</p>");
+        document.write("adalah <b>Hendri Mei Singgih</b>")
+    </script>
+</head>
+<body>
+
+</body>
+</html>

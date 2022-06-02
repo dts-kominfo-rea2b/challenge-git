b9762342c24071017655cbd086267b8a94bf83e3
hasanex328 <pajak@NB170509BGU0303.intranet.pajak.go.id>
2022-06-02 15:41:13 +0700

    coba commit lagi, yg tadi gagal

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log ("halo, namaku hasan");
+// function multiply (number1, number2){
+//    return number1 * number2
+// }
+// const hasil = multiply(3,4)
+// console.log (hasil);
+
+
+// function getUsers() {
+//     // dapet data dari database
+//     const users = [
+//         {'nama':'hasan','alamat':'kentingan'},
+//         {'nama':'budi' , 'alamat':'ngoresan'}
+//     ]
+//     return users
+// }
+
+// const ecommersUsers = getUsers();
+// console.log (`nama pelanggan ${ecommersUsers[0].alamat}`);
+
+
+
+// const GetStudents =  () => {
+//     return [
+//         {'nama': 'banu', 'umur':12},
+//         {'nama': 'siti', 'umur':11}
+//     ]
+    
+// }
+
+
+// const DataStudents = GetStudents();
+// console.log (DataStudents[0].nama);
+
+
+// const average = (nums) => {
+//     let total = 0;
+//     for (const Element of nums){
+//         total += Element;
+//     }
+//     return total / nums.length;
+// }
+
+// const hasil = average([3,4,5,6]);
+// const hasil2 = average([3,4,34,6]);
+// const hasil3 = average([3,78,5,6]);
+// console.log(hasil);
+// console.log(hasil2);
+// console.log(hasil3);
+
+((nomber1,nomber2) => {console.log(nomber1*nomber2)})(2,8);
